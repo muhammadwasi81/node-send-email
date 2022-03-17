5a82e40d4695b35a93aa6e4f6e189590d6b24a22
rimsha117 <rimsha.techtory@gmail.com>
2022-03-17 15:34:38 +0500

    done: send email

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,43 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const mg = require('mailgun.js');
-
-const app = express();
+const mg = require('mailgun-js');
 
 dotenv.config();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+const mailgun = () =>
+  mg({
+    apiKey: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMIAN,
+  });
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/api/email', (req, res) => {
+  const { email, subject, message } = req.body;
+  mailgun()
+    .messages()
+    .send(
+      {
+        from: 'Wasiarain819@gmail.com',
+        to: `${email}`,
+        subject: `${subject}`,
+        html: `<p>${message}</p>`,
+      },
+      (error, body) => {
+        if (error) {
+          console.log(error);
+          res.status(500).send({ message: 'Error in sending email' });
+        } else {
+          console.log(body);
+          res.send({ message: 'Email sent successfully' });
+        }
+      }
+    );
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(() => {
-  console.log(`server at http://localhost:${PORT} successfully started`);
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
 });
--- src/App.js
+++ src/App.js
@@ -1,22 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (!email || !subject || !message) {
+      return toast.error('Please fill email, subject and message');
+    }
+    try {
+      setLoading(true);
+      const { data } = await axios.post(`/api/email`, {
+        email,
+        subject,
+        message,
+      });
+      setLoading(false);
+      toast.success(data.message);
+    } catch (err) {
+      setLoading(false);
+      toast.error(
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message
+      );
+    }
+  };
   return (
     <div className="App">
+      <ToastContainer position="bottom-center" limit={1} />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={submitHandler}>
+          <h1>Send Email</h1>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+            ></input>
+          </div>
+          <div>
+            <label htmlFor="subject">Subject</label>
+            <input
+              id="subject"
+              type="text"
+              onChange={(e) => setSubject(e.target.value)}
+            ></input>
+          </div>
+          <div>
+            <label htmlFor="message">Message</label>
+            <textarea
+              id="message"
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
+          </div>
+          <div>
+            <label></label>
+            <button disabled={loading} type="submit">
+              {loading ? 'Sending...' : 'Submit'}
+            </button>
+          </div>
+        </form>
       </header>
     </div>
   );
